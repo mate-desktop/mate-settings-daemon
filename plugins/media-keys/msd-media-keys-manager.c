509d3708f1c0c35956e214e8ba3d04f8408cd69c
Friedrich Herbst <frimam@web.de>
2016-01-08 17:33:51 +0100

    Add setting to enable/disable the osd

--- plugins/media-keys/msd-media-keys-manager.c
+++ plugins/media-keys/msd-media-keys-manager.c
@@ -410,6 +410,10 @@ dialog_show (MsdMediaKeysManager *manager)
         gtk_window_set_screen (GTK_WINDOW (manager->priv->dialog),
                                manager->priv->current_screen);
 
+        /* Return if OSD notifications are disabled */
+        if (!g_settings_get_boolean (manager->priv->settings, "enable-osd"))
+                return;
+
         /*
          * get the window size
          * if the window hasn't been mapped, it doesn't necessarily
